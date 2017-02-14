de08be8abdbff74b60fc1149c4c962c3132c22ea
Julian Domingo <julian.domingo@utexas.edu>
2017-02-14 15:08:13 -0600

    Added concurrent stack with locks

---
+++ Examples/concurrentDataStructures/withLocks/ConcurrentQueue.java
@@ -0,0 +1,65 @@
+// Julian Domingo
+import java.util.concurrent.*;
+
+public class ConcurrentQueue<T> {
+	private ReentrantLock enqueueLock;
+	private ReentrantLock dequeueLock;
+
+	private Node<T> head;
+	private Note<T> tail;
+
+	public ConcurrentQueue<T>() {
+		enqueueLock = new ReentrantLock();
+		dequeueLock = new ReentrantLock();
+		head = new Node<T>(null);
+		head = tail;
+	}
+
+	class Node<T> {
+		private T value;
+		private Node<T> next;
+		
+		public Node<T>(T value) {
+			this.value = value;
+		}
+		
+		public T getValue() {
+			return value;
+		}
+	}
+
+	public void enqueue(T value) {
+		if (value == null) {
+			throw new NullPointerException();
+		}
+		enqueue.lock();
+		try {
+			Node<T> newNode = new Node<T>(value);
+			tail.next = newNode;
+			tail = newNode;
+		}
+		finally {
+			enqueue.unlock();
+		}
+	}
+
+	public T dequeue() {
+		T popped;
+		dequeueLock.lock();
+		try {
+			if (isEmpty()) {
+				throw new EmptyException();
+			}
+			popped = head.getValue();
+			head = head.next;
+		}
+		finally {
+			dequeue.unlock();
+		}
+		return popped;
+	}
+		
+	public boolean isEmpty() {
+		return head.next == null;
+	}
+}
