9fe7de89ae3705e9e4b1dde6d0be16e96c0e2bc5
apb973 <apb973@users.noreply.github.com>
2017-03-23 16:57:41 -0500

    Update Client.java

--- project4/Client.java
+++ project4/Client.java
@@ -8,61 +8,59 @@ import java.net.*;
 import java.io.*;
 
 public class Client {
-    private static ArrayList<InetSocketAddress> servers; 
-    private static int serverInstances;
+    private ArrayList<InetSocketAddress> servers; 
+    private int serverInstances;
 
     public static void main (String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter total number of servers: ");
-        int serverInstances = scanner.nextInt();
-        System.out.println("");
+        Client client = new Client();
+        Scanner scanner = new Scanner(System.in);   
+        client.serverInstances = scanner.nextInt();
+        scanner.nextLine();
+        
+        client.servers = new ArrayList<InetSocketAddress>(client.serverInstances); 
 
-        servers = new ArrayList<InetSocketAddress>(serverInstances); 
-
-        for (int server = 0; server < serverInstances; server++) { 
-            addNextServerFrom(scanner);
+        for (int server = 0; server < client.serverInstances; server++) { 
+            client.addNextServerFrom(scanner);
         }
 
         while (scanner.hasNextLine()) {
-            System.out.print("Enter a command: ");
-            String command = scanner.next();
-            String serverResponse = execute(command);
-            System.out.println(serverResponse);
-            System.out.println("");
+            String command = scanner.nextLine();
+            String serverResponse = client.execute(command); 
+            System.out.println(serverResponse); 
         }
+        scanner.close();
     }
 
-    private static String send(InetSocketAddress server, String command) {
+    private String send(InetSocketAddress server, String command) {
         String responseOfTCPServer;
-        PrintWriter toServer;
-        BufferedReader fromServer;
+        PrintStream printStream;
+        Scanner scanner;
         Socket clientSocket;
 
         while (true) {
             try {
-                clientSocket = new Socket(servers.get(0).getAddress(), servers.get(0).getPort());
-                //clientSocket.connect(servers.get(0), 100);
-                //clientSocket.setSoTimeout(100);
-                fromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                toServer = new PrintWriter(clientSocket.getOutputStream());
-                toServer.println(command);
-                toServer.flush();
-
-                if (!isTimedOut(fromServer, 100)) {
-                    responseOfTCPServer = fromServer.readLine();
-                    return responseOfTCPServer;
-                }
-                else {
-                    deprecateServer();
-                }
-
+                clientSocket = new Socket(servers.get(0).getHostName(),servers.get(0).getPort());
+                clientSocket.setSoTimeout(100);
+                scanner = new Scanner(clientSocket.getInputStream());
+                printStream = new PrintStream(clientSocket.getOutputStream());
+                printStream.println(command);
+                printStream.flush();
+                do
+                {
+                	responseOfTCPServer = scanner.nextLine();
+                }while(responseOfTCPServer.equals("alive"));
+                scanner.close();
                 clientSocket.close();
+                return responseOfTCPServer;
+            }
+            catch (NoSuchElementException e)
+            {
+            	deprecateServer();
             }
             catch (SocketTimeoutException e) {
                 deprecateServer();
             }
             catch (ConnectException e) {
-                e.printStackTrace();
                 deprecateServer();
             }
             catch (IOException e) {
@@ -71,46 +69,29 @@ public class Client {
         }
     }  
 
-    private static boolean isTimedOut(BufferedReader fromServer, int timeOut) {
-        long startTime = System.currentTimeMillis();
-        try {
-            while (!fromServer.ready()) {
-                long currentTime = System.currentTimeMillis();
-                if (currentTime - startTime >= (long) timeOut) {
-                    return true;
-                }
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    private static void deprecateServer() {
+    private void deprecateServer() {
         servers.remove(0);
         serverInstances--;
     }  
     
-    private static String execute(String command) {
+    private String execute(String command) {
         String[] tokens = command.split(" ");
         return (isValidCommand(tokens[0])) ? sendToTCPServer(command) : "ERROR: No such command";        
     }
         
-    private static boolean isValidCommand(String commandArgument) {
+    private boolean isValidCommand(String commandArgument) {
         // "(?i)" denotes case insensitivity.
         return commandArgument.matches("(?i)purchase|cancel|list|search");
     } 
     
-    private static void addNextServerFrom(Scanner scanner) {
-        System.out.print("Enter a server in the form '<IP Address>:<Port Number>': ");
-        String[] serverInformation = scanner.next().split(":");
+    private void addNextServerFrom(Scanner scanner) {
+        String[] serverInformation = scanner.nextLine().split(":");
         String IPAddress = serverInformation[0];
         int portNumber = Integer.parseInt(serverInformation[1]); 
         servers.add(new InetSocketAddress(IPAddress, portNumber));
     }   
     
-    private static String sendToTCPServer(String command) {
+    private String sendToTCPServer(String command) {
         // Assumed majority of servers will not crash, so "severs.get(0)" will never fail.
         String serverResponse = send(servers.get(0), command);
         if (command.split(" ")[0].matches("(?i)list|search")) {
@@ -119,7 +100,7 @@ public class Client {
         return serverResponse;
     }   
 
-    private static String reformatServerResponse(String serverResponse) {
+    private String reformatServerResponse(String serverResponse) {
         String reformattedResponse = "";
         String[] items = serverResponse.split("\\$");
 
